9ebfb64aa50d2bb4ef056bb187105102ca200957
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2018-01-07 13:35:28 +0100

    Documentation, Documentation, Documentation...

--- src/app/util/StreamsParser.js
+++ src/app/util/StreamsParser.js
@@ -1,3 +1,10 @@
+/**
+ * Provides parsed and ready-to-use data to the 'Live Now' page.
+ * Mainly list of live streams to iterate through and render into
+ * videos.
+ * @type {JSONParser}
+ */
+
 import JSONParser from './JSONParser';
 
 export default class StreamsParser {
@@ -6,14 +13,23 @@ export default class StreamsParser {
     this.streamsList = [];
   }
 
+  /**
+   * Gives us a list of streams to render later into watchable videos.
+   * Side note: of course here we pretend that there is a server providing
+   * us with the data so we take all links for granted.
+   * @return {Array} List of streams marked as currently live.
+   */
   constructStreamArray() {
-    this.jsonParser.loadJSONFile(`${this.jsonParser.BASE_URL}streams.json`, res => {
+    let self = this;
+    this.jsonParser.loadJSONFile(`${this.jsonParser.BASE_URL}streams.json`, _constructStreamArray);
+    function _constructStreamArray(res) {
       for (var i = 0; i < res.length; i++) {
         if (res[i].live === 'true') {
-          this.streamsList.push(res[i].manifest);
+          self.streamsList.push(res[i].manifest);
         }
       }
-    });
+      console.log(self.streamsList);
+    }
     return this.streamsList;
   }
 }
--- src/app/util/misc.js
+++ src/app/util/misc.js
@@ -6,6 +6,13 @@ export default class Misc {
   constructor() {
 
   }
+
+  /**
+   * Generic function where we pass a number of Bytes and get the
+   * most suitable conversion whether KB, MB, GB or TB.
+   * @param  {Number} bytes Given bytes.
+   * @return {Number}       Conversion result.
+   */
   convertBytes(bytes) {
    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
    if (bytes == 0) return '0 Byte';
